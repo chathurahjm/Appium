ab2a639d881bebcf46ef155af0241989cadc7dec
chathurahjm <chathudagr8@yahoo.com>
2023-02-19 20:58:49 +0530

    Add files via upload

---
+++ src/test/java/userLoginTest.java
@@ -0,0 +1,46 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class userLoginTest {
+
+
+    private AndroidDriver driver;
+
+
+    @BeforeSuite
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("appium:deviceName", "AOSP on IA Emulator");
+        desiredCapabilities.setCapability("appium:platformVersion", "9");
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("appium:appPackage", "com.iitdh.sonusourav.instigo");
+        desiredCapabilities.setCapability("appium:appActivity", ".Account.SplashActivity");
+        desiredCapabilities.setCapability("appium:automationName", "Appium");
+        desiredCapabilities.setCapability("appium:noReset", false);
+        desiredCapabilities.setCapability("appium:ensureWebviewsHavePages", true);
+        desiredCapabilities.setCapability("appium:nativeWebScreenshot", true);
+        desiredCapabilities.setCapability("appium:newCommandTimeout", 3600);
+        desiredCapabilities.setCapability("appium:connectHardwareKeyboard", true);
+
+        URL remoteUrl = new URL("http://localhost:4723/wd/hub");
+
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+    }
+
+    @Test
+    public void sampleTest() {
+
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
